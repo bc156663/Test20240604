98ff3ef3dbcc956b719b8608976d8aedca921ca2
Caitlin <156647080+bc156663@users.noreply.github.com>
2024-06-04 08:51:52 +0000

    Testaufgaben bearteitet

--- klausur/klausur20240604.js
+++ klausur/klausur20240604.js
@@ -7,6 +7,11 @@ console.log("Klausur 20240604")
 console.log("Aufgabe 1")
 // 1a) Berechnen Sie das Volumen eines Würfels mit Hilfe der Grundrechenoperatoren (+ , - , * , /) , bei dem die Kantenlänge mit der Varablen a festgelegt wird.
 
+let a= 4;
+let b= 4;
+let c= 4;
+
+console.log('Das Volumen eines Würfels mit Katenenlängen von jeweils 4 cm beträgt: '+(a*b*c)+'cm^3');
 
 
 // 1b) - Nur Klausurschreiber
@@ -25,6 +30,8 @@ console.log("Aufgabe 2")
 // 2a)
 // Testschreiber deklarieren pi und initialisieren pi mit dem Wert 3.14.
 
+let pi=3.14;
+
 
 
 // Klausurschreiber deklarieren pi und initialisieren pi mit dem Wert aus der bekannten Bibliothek namens Math. Geben Sie Math.PI an, um den Wert von Pi in einer Variablen speichern zu können.
@@ -35,6 +42,11 @@ console.log("Aufgabe 2")
 // 2b)
 // Berechnen Sie Flächeninhalt.
 
+let radius= 2;
+let durchmesser= 2*radius;
+
+console.log('Der Flächeninhalt eines Kreises mit einem Radius von 2 cm beträgt: '+(pi*durchmesser)+'cm^2');
+
 
 
 
@@ -42,12 +54,19 @@ console.log("Aufgabe 2")
 // 2c)
 // Berechnen Sie den Umfang
 
+let umfang=2*pi*radius;
+
+console.log('Der Umfang des Kreises beträgt: '+(umfang)+'cm');
+
+
 
 
 
 
 // 2d)
 // Berechnen Sie den Durchmesser
+
+console.log('Der Durchmesser des Kreises beträgt: '+(durchmesser)+'cm');
 
 
 
@@ -68,15 +87,29 @@ console.log("Aufgabe 3")
 // 3a)
 // Berechnen Sie den Rabatt in einer sprechenden Variablen. Geben Sie einen Anwortsatz auf der Konsole aus.
 
+let listenpreis= 16780;
+let prozentsatz= 0.05;
+let rabatt= listenpreis*prozentsatz;
+
+console.log('Der Rabatt beträgt: '+(rabatt)+'EUR.');
+
 
 
 // 3b)
 // Berechnen Sie den Netto-Rechnungsbetrag in einer sprechenden Variablen. Geben Sie einen Anwortsatz auf der Konsole aus.
 
+let nettorechnungsbetrag= listenpreis-rabatt;
+
+console.log('Der Netto-rechnungsbetrag beträgt: '+(nettorechnungsbetrag)+'EUR.');
 
 
 // 3c)
 // Berechnen Sie die MwSt in einer sprechenden Variablen. Geben Sie einen Anwortsatz auf der Konsole aus.
+ 
+let mwstprozentsatz=0.19;
+let mwst= nettorechnungsbetrag*mwstprozentsatz;
+
+console.log('Die Mehrwertssteuer beträgt: '+(mwst)+'EUR.');
 
 
 
@@ -109,6 +142,12 @@ console.log("Aufgabe 4")
 // 4a)
 // Ein Kunde nimmst bei einer Bank einen Kredit über 5000 € auf. Die Laufzeit beträgt 6 Monate bei einem Zinssatz von 10%. Berechne die Zinszahlungen, die in diesem Zeitraum anfallen. Arbeiten Sie mit Variablen. Antwortsatz!
 
+let kapital= 5000;
+let zinssatz= 0.10;
+let verzinsungsszeit= 90;
+let zinsbetrag= kapital*(zinssatz/100)*(verzinsungsszeit/360);
+
+console.log('Die Zinszahlungen, die für den Kunden innerhalb von 6 Monaten anfallen betragen: '+(zinsbetrag)+'EUR.');
 
 
 
@@ -133,18 +172,23 @@ console.log("Aufgabe 5")
 // Erstellen Sie die notwendigen Variablen und weisen Sie beispielhaft Werte zu. 
 // Erstellen Sie zusätzlich eine Variable für den Namen der Person.
 
+let m= 68;
+let l= 170;
+let name= "Pit";
+
+
 
 
 
 // 5b)
 // Führen Sie die Berechnung durch.
 
-
+let bmi= m/(l*l);
 
 // 5c) 
 // Geben Sie den Antwortsatz nach folgendem Muster aus. Die Werte werden aus den Variablen und der vorherigen Berechnung erzeugt: "Pit hat bei einer Körpergröße von 170 cm und einem Körpergewicht von 68 kg einen BMI von 24."
 
-
+console.log( name +' hat mit einer Körpergröße von ' +(l)+ ' cm '+' und einem Körpergewicht von '+(m)+' kg '+ ' einen BMI von '+(bmi)+' .');
 
 
 // 5d - Nur Klausurschreiber
